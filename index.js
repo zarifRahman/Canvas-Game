811c34179d493ea9e414422e0699fba43739ff7e
zarifRahman <zarif.shahriar.rahman@gmail.com>
2022-04-27 16:15:07 +0600

    create enemies

--- index.js
+++ index.js
@@ -47,6 +47,27 @@ class Projectile {
     this.y = this.y + this.velocity.y
   }
 }
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
 // set canvas position in center
 const x = canvas.width / 2;
 const y = canvas.width / 2;
@@ -54,6 +75,33 @@ const y = canvas.width / 2;
 const player = new Player(x, y, 30, 'blue');
 
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies(){
+  setInterval(() => {
+    const radius = Math.random() * (50 - 6) + 6;
+    let x
+    let y
+    if (Math.random() < 0.5) {
+       x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+       y = Math.random() * canvas.height;
+    } else {
+       x = Math.random() * canvas.width;
+       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+    const color = 'green';
+    const angle = Math.atan2(
+      canvas.height / 2 - y,
+      canvas.width / 2 - x
+    );
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+    console.log(enemies);
+  },1000)
+}
 
 function animate(){
   requestAnimationFrame(animate);
@@ -62,6 +110,7 @@ function animate(){
   projectiles.forEach(projectile => {
     projectile.update();
   })
+  enemies.forEach(enemy => enemy.update());
 }
 
 window.addEventListener('click', (event)=> {  
@@ -73,10 +122,10 @@ window.addEventListener('click', (event)=> {
     x: Math.cos(angle),
     y: Math.sin(angle)
   }
-  console.log(angle);
   projectiles.push(
     new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
   );
 })
 
 animate();
+spawnEnemies();
